0b47944cc91ecb0c9217628887e24150c85af1f2
Diogo Costa Hutner <diogohutner@gmail.com>
2022-10-16 09:20:42 -0300

    .
    .

--- Inicio.R
+++ Inicio.R
@@ -108,7 +108,7 @@ portfolio_grande_retornos <- as.timeSeries(log(lag(banco_dados_estimacao[,portfo
 spec <- portfolioSpec()
 setNFrontierPoints(spec) <- 100
 setRiskFreeRate(spec) <- mean(banco_dados_estimacao[,'selic']/10)
-?setTargetReturn
+
 
 portfolio_pequeno.fronteira <- portfolioFrontier(portfolio_pequeno_retornos,spec)
 
@@ -214,8 +214,40 @@ resultado_grande[,portfolio_grande] <- round(getPortfolio(portfolio_grande.front
 plot(resultado_grande[,c("VaR","mean")])
 
 
-write.csv("resultado_pequeno.csv",resultado_pequeno,sep=';',dec=',')
-write.csv("resultado_medio.csv",resultado_medio,sep=';',dec=',')
-write.csv("resultado_grande.csv",resultado_grande,sep=';',dec=',')
-getwd()
-?portfolioFrontier
+write.table(resultado_pequeno,"resultado_pequeno.csv",sep=';',dec=',')
+write.table(resultado_medio,"resultado_medio.csv",sep=';',dec=',')
+write.table(resultado_grande,"resultado_grande.csv",sep=';',dec=',')
+
+#### Analise do Ibovespa ####
+
+var_ibov <- as.timeSeries(log(lag(banco_dados_estimacao[,"X.5EBVSP"])/banco_dados_estimacao[,"X.5EBVSP"]))
+
+portfolio_ibov <- c("RRRP3.SA","ALPA4.SA","ABEV3.SA","AMER3.SA",
+                 "ARZZ3.SA","ASAI3.SA","AZUL4.SA","B3SA3.SA","BPAN4.SA",
+                 "BBSE3.SA","BRML3.SA","BBDC3.SA","BBDC4.SA","BRAP4.SA",
+                 "BBAS3.SA","BRKM5.SA","BRFS3.SA","BPAC11.SA","CRFB3.SA",
+                 "CCRO3.SA","CMIG4.SA","CIEL3.SA","COGN3.SA","CPLE6.SA",
+                 "CSAN3.SA","CPFE3.SA","CMIN3.SA","CVCB3.SA","CYRE3.SA",
+                 "DXCO3.SA","ECOR3.SA","ELET3.SA","ELET6.SA","EMBR3.SA",
+                 "ENBR3.SA","ENGI11.SA","ENEV3.SA","EGIE3.SA","EQTL3.SA",
+                 "EZTC3.SA","FLRY3.SA","GGBR4.SA","GOAU4.SA","GOLL4.SA",
+                 "NTCO3.SA","SOMA3.SA","HAPV3.SA","HYPE3.SA","IGTI11.SA",
+                 "IRBR3.SA","ITSA4.SA","ITUB4.SA","JBSS3.SA","KLBN11.SA",
+                 "RENT3.SA","LWSA3.SA","LREN3.SA","MGLU3.SA","MRFG3.SA",
+                 "CASH3.SA","BEEF3.SA","MRVE3.SA","MULT3.SA","PCAR3.SA",
+                 "PETR3.SA","PETR4.SA","PRIO3.SA","PETZ3.SA","POSI3.SA",
+                 "QUAL3.SA","RADL3.SA","RAIZ4.SA","RDOR3.SA","RAIL3.SA",
+                 "SBSP3.SA","SANB11.SA","SMTO3.SA","CSNA3.SA","SLCE3.SA",
+                 "SULA11.SA","SUZB3.SA","TAEE11.SA","VIVT3.SA","TIMS3.SA",
+                 "TOTS3.SA","UGPA3.SA","USIM5.SA","VALE3.SA","VIIA3.SA",
+                 "VBBR3.SA","WEGE3.SA","YDUQ3.SA")
+
+portfolio_ibov.retornos <- as.timeSeries(log(lag(banco_dados_estimacao[,portfolio_ibov])/banco_dados_estimacao[,portfolio_ibov]))
+portfolio_ibov.have_retornos <- portfolio_ibov.retornos[,!as.logical(colSums(is.na(portfolio_ibov.retornos)))]
+portfolio_ibov.frontiers <- portfolioFrontier(portfolio_ibov.have_retornos,spec)
+
+frontierPlot(portfolio_ibov.frontiers, col = c('blue', 'red'), pch = 20,
+             risk="VaR", title = F)
+
+points(var(var_ibov),mean(var_ibov),col="Black")
+mean(banco_dados_estimacao[,'selic'])
